2db7d256df6afde7363036e76bb918bb05b95e17
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-08-15 16:53:39 -0300

    att

---
+++ aula 5-variaveis/index.js
@@ -0,0 +1,42 @@
+let a = 10// usamos o termo let que é uma variavel para atribuirmos dados de entrada, tais como numeros ou textos que podem ser alterados.
+let b = 10// 
+console.log(b)//comando no qual utilizamos para imprimir informações ao console, nesse exemplo será impresso a varaivel b.
+
+b = 5// varaivel let b tenso seu valor alterado.
+console.log(a, b)
+
+
+
+
+
+
+/*
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+*/
+
+/*
+let horasTrabalhadas = prompt ("Quantas horas voce trabalha por dia?")// utilizando a varaivel let inserimos dados de entrada por meio de um prompt
+let valorRecebido = prompt("Quanto voce recebe por dia?")// o comando prompt nos gera uma caixa de pop-up para inserirmos um tipo de dado como resposta
+console.log("Voce recebe", horasTrabalhadas, "/", valorRecebido, "por hora")// o comando console.log nos devolve a resposta de saída conforme inserido na caixa de prompt
+*/
+
+let identificação =  ""
+identificação = prompt("Qual é o seu nome?")
+console.log(typeof identificação)
+
+let idade = ""
+idade = prompt ("qual é a sua idade?")
+console.log(typeof idade)
+/*o comando esta nos dando string como resposta por nao ter nenhum dado inserido na variavel, somente as aspas vazias.*/
+
+console.log("Meu nome é:", identificação, "e eu tenho", idade, "anos")
+
+ /*usamos o prompt para perguntar ao usuario uma determinada pergunta*/
+
+
+
