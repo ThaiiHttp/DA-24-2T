ec40c0fb7cbcef258371b5f1567789227debd7a8
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-08-15 14:01:21 -0300

    att

---
+++ Trabalho avaliativo/Validação de algoritmos.js
@@ -0,0 +1,16 @@
+ = 2
+const numP4 = 4
+const numP6 = 6
+const numP8 = 8
+const numP10 = 10
+
+const numP2= prompt ("quais são os numeros pares?")
+if(nump2%2==0){
+    console.log("par")
+}
+if(nump2%2==1){
+    console.log("impar")
+}
+//console.log ("os numeros pares são:" , numP2, numP4, numP6, numP8, "e" numP10)
+
+git PushManager
---
+++ aula3/index.js
@@ -0,0 +1,41 @@
+//texto exemplo
+//texto exemplo2 
+/*comentario adc*/
+
+
+console.log("Ola Mundo!")
+
+console.log("Olá turma 24 2T")
+
+prompt("qual o seu nome?")
+  
+const nome = "Thaissa"
+
+const idade = 40
+console.log("Ola", "meu nome é:" , nome, "e eu tenho:" , idade, "anos");
+
+const nome1 = "Thaissa"
+
+const sobrenome = "Nunes"
+
+const idade1 = 21
+console.log("Ola", "meu nome é:" , nome1, sobrenome, "e eu tenho:" , idade1, "anos");
+
+
+const got = "Game of Thrones"
+const TemporadasdeGOt = "8";
+
+console.log (typeof got)
+console.log (typeof temporadasdeGOt)
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 2
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
+
+
+
