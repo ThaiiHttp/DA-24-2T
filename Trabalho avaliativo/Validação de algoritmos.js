118277ce6ecefc167c6f6f976f2a74285595edd2
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-08-19 14:33:17 -0300

    att

--- Trabalho avaliativo/Validação de algoritmos.js
+++ Trabalho avaliativo/Validação de algoritmos.js
@@ -1,10 +1,10 @@
- = 2
-const numP4 = 4
-const numP6 = 6
-const numP8 = 8
-const numP10 = 10
+const kk =""
+const numP4=""
+const numP6=""
+const numP8=""
+const numP10=""
 
-const numP2= prompt ("quais são os numeros pares?")
+const numP2= prompt ("Digite um número")
 if(nump2%2==0){
     console.log("par")
 }
@@ -12,5 +12,3 @@ if(nump2%2==1){
     console.log("impar")
 }
 //console.log ("os numeros pares são:" , numP2, numP4, numP6, numP8, "e" numP10)
-
-git PushManager
