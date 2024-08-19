08b32b4a7d62bbfd0c5b1ec7e41fbb2a1b2ceb5a
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-08-19 17:05:32 -0300

    att

--- aula6/index.js
+++ aula6/index.js
@@ -1,1 +1,100 @@
+//const primeiroValor = 10
+//const segundoValor = 20
 
+//const resultado = primeiroValor + segundoValor + 4
+
+//console.log("O resultado é:", resultado)
+
+//const primeiroValor = 10
+//const segundoValor = 5
+
+//const resultado = primeiroValor - segundoValor
+//console.log("O resultado é:", resultado)
+
+/*Exemplo de multiplicação*/
+
+//const primeiroValor = 10
+//const segundoValor = 5
+
+//const resultado = primeiroValor * segundoValor
+//console.log ("O resultado é:", resultado)
+
+/*Exemplo de divisão*/
+
+//const primeiroValor = 10
+//const segundoValor = 5
+
+//const resultado = primeiroValor / segundoValor
+//console.log("o resultado é:", resultado)
+
+/*exemplo de resto de divisão*/
+
+//const primeiroValor = 200
+//const segundoValor = 10
+//const resultado = primeiroValor / segundoValor
+//console.log ("O resultado é:", resultado)
+
+/*const s1=3
+const s2=4
+const resultado=s1+s2
+console.log("O resultado é", resultado)
+
+const m3=3
+const m5=5
+const resultado2=m3*m5/2
+console.log("O resultado é:", resultado2)
+
+const s5=5
+const s4=4
+const resultado3=s5-s4
+const reslt=resultado3*(-1)
+console.log ("O resultado é", resultado3)
+
+const resto=234%5
+console.log("O rsultado é:", resto)*/
+
+//exemplo comparadores iguais
+
+/*const condicao=1==="1" //resultado=false//
+console.log(condicao)
+
+/*Exemplo comparadores diferentes*/
+
+//const condicao=1!==2
+//console.log(condicao)
+/*resultado vai ser true porque sao realmente diferentes*/
+
+/*exemplo de comparador maior ou maior igual*/
+//const condicao = 1 > 2 
+//console.log(condicao) // resultado será false
+//const condicao1 = 2 > 2
+//console.log (condicao1) // resultado será false
+//const condicao2 = 2>=2
+//console.log (condicao2) //resultado será true
+//const condicao3 = 3>2
+//console.log(condicao3) // resultado será true
+
+/*exemplo de comparador menor ou menor igual*/
+//const condicao=1<2
+//console.log (condicao) //resultado true
+//const condicao1 = 2 < 2
+//console.log(condicao1) // resultado false
+//const condicao2 = 2 <= 2
+//console.log (condicao2) // resultado true
+//const condicao3 = 3 < 2 
+//console.log(condicao3)// resultado false
+
+const dados1 = 2
+const dados2 = 4
+
+const condicao = dados1 === dados2
+console.log (condicao) // false
+
+const condicao2 = dados1 !== dados2
+console.log (condicao2) // true
+
+const condicao3 = dados1 > dados2 
+console.log (condicao3) // false
+
+const condicao4 = dados1 < dados2 
+console.log (condicao4) // true
