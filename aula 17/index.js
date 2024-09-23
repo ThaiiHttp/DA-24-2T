ef12836db1d2ae2259049c9c02170fdd976a0b8b
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-09-23 14:34:42 -0300

    add

--- aula 17/index.js
+++ aula 17/index.js
@@ -3,7 +3,7 @@
 const professor = {}//utilizamos as chaves para representar a estrutura de um objeto
 
 const professor = {
-    nome : 'Maria' //Dentro das chaves, podemos criar propriedades contendo chave e valo
+    nome : 'Maria' //Dentro das chaves, podemos criar propriedades contendo chave e valor
     } 
-    
+
 
