2ce145715eb161f0049ac8cb84f59c212fc7d945
luizaorquiz <thaissafernandesnunes02@gmail.com>
2024-09-23 14:41:44 -0300

    add

--- aula 17/index.js
+++ aula 17/index.js
@@ -4,6 +4,6 @@ const professor = {}//utilizamos as chaves para representar a estrutura de um ob
 
 const professor = {
     nome : 'Maria' //Dentro das chaves, podemos criar propriedades contendo chave e valor
-    } 
-
-
+    
+/* Os valores de uma chave também podem ser arrays e funções
+(nesse caso, métodos)*/
