b6e359056a4e263f5269481f84b553f512e543e1
ThaiiHttp <thai@MacBook-Air-de-Thaissa.local>
2024-09-24 14:49:00 -0300

    now

---
+++ aula 20/index.js
@@ -0,0 +1,19 @@
+let matriz = [
+    [1,2,3],
+    [4,5,6]
+]
+console.log(matriz[1][2]) //6
+
+let matrizes = [
+    [10,20,30],
+    [40,50,60],
+    [70,80,90]
+]
+ //Exercicio
+ let matriz1 = [
+    [0,0,0],
+    [0,0,0],
+    [0,1,0]
+ ]
+ console.log(matriz1[2][1])
+console.log(matriz1)
